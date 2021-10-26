fead5a9c1fa63f64a813d327a99d919bc93fbb78
redivv <j.rajca45@gmail.com>
2021-10-26 22:49:08 +0200

    react-boostrap installed with demo modal

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, Fragment, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import CategoriesRequestService from "../services/CategoriesRequestService";
 import AuthContext from "../store/auth-context";
 
@@ -10,7 +11,31 @@ const Main: React.FC = () => {
       .catch((error) => console.log(error.response));
   }, [authContext.tokenObject]);
 
-  return <button onClick={authContext.logout}>Penis</button>;
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Fragment>
+      <button onClick={authContext.logout}>Logout</button>
+      <button onClick={handleShow}>Modal</button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Fragment>
+  );
 };
 
 export default Main;
